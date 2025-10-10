99c7dbc65d1e1e0cb2a7a651a5f2d332ceb2704d
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-10-10 13:41:07 +0300

    added exception

--- LogAn/LogAnalyzer.cs
+++ LogAn/LogAnalyzer.cs
@@ -4,6 +4,9 @@ public class LogAnalyzer
 {
     public bool IsValidLogFileName(string fileName)
     {
+        if (string.IsNullOrEmpty(fileName))
+            throw new ArgumentException("filename has to be provided");
+
         if (!fileName.EndsWith(".SLF",
             StringComparison.CurrentCultureIgnoreCase))
             return false;
