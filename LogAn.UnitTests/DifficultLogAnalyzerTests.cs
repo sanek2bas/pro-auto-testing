582a20cfcb990dc26bab7acf0025fb8d3ebe5689
sanek2bas <sanek_2bas@mail.ru>
2025-10-25 23:42:46 +0300

    test with exception

--- LogAn.UnitTests/DifficultLogAnalyzerTests.cs
+++ LogAn.UnitTests/DifficultLogAnalyzerTests.cs
@@ -16,12 +16,43 @@
             Assert.True(result);
         }
 
+        [Fact]
+        public void IsValidFileName_ExtManagerThrowsException_ReturnsFalse()
+        {
+            FakeExtensionManager myFakeManager = 
+                new FakeExtensionManager();
+            string exMessage = "it's a fake copy";
+            myFakeManager.WillThrow = new Exception(exMessage);
+
+            DifficultLogAnalyzer log =
+                new DifficultLogAnalyzer(myFakeManager);
+
+            bool result;
+            string message = string.Empty;
+            try
+            {
+                result = log.IsValidLogFileName("anything.anyextension");
+            }
+            catch(Exception exception)
+            {
+                result = false;
+                message = exception.Message;
+            }
+
+            Assert.False(result);
+            Assert.Contains(message, exMessage);
+        }
+
         internal class FakeExtensionManager : IExtensionManager
         {
             public bool WillBeValid { get; set; }
 
+            public Exception WillThrow { get; set; }
+
             public bool IsValid(string fileName)
             {
+                if (WillThrow != null)
+                    throw WillThrow;
                 return WillBeValid;
             }
         }
