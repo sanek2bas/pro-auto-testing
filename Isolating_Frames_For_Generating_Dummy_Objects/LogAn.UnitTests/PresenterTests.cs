7a5ad83c0c13d6e97a574d75301c1a1797f337be
sanek2bas <sanek_2bas@mail.ru>
2026-01-27 17:43:25 +0300

    testing the event listener.

---
+++ Isolating_Frames_For_Generating_Dummy_Objects/LogAn.UnitTests/PresenterTests.cs
@@ -0,0 +1,28 @@
+﻿using NSubstitute;
+
+namespace LogAn.UnitTests;
+
+public class PresenterTests
+{
+    [Fact]
+    public void Ctor_WhenViewIsLoaded_CallsViewRender()
+    {
+        var mockView = Substitute.For<IView>();
+
+        Presenter p = new Presenter(mockView, Substitute.For<ILogger>());
+        mockView.Loaded += Raise.Event<Action>();
+        mockView.Received()
+            .Render(Arg.Is<string>(s => s.Contains("Hello World")));
+    }
+
+    [Fact]
+    public void Ctor_WhenViewhasError_CallsLogger()
+    {
+        var stubView = Substitute.For<IView>();
+        var mockLogger = Substitute.For<ILogger>();
+
+        Presenter p = new Presenter(stubView, mockLogger);
+        stubView.ErrorOccured +=
+            Raise.Event<Action<string>>("fake error");
+    }
+}
---
+++ Isolating_Frames_For_Generating_Dummy_Objects/LogAn/Presenter.cs
@@ -0,0 +1,32 @@
+﻿namespace LogAn;
+
+public interface IView
+{
+    event Action Loaded;
+    event Action<string> ErrorOccured;
+    void Render(string text);
+}
+
+public class Presenter
+{
+    private readonly IView view;
+    private readonly ILogger log;
+
+    public Presenter(IView view, ILogger log)
+    {
+        this.view = view;
+        this.log = log;
+        this.view.Loaded += OnLoaded;
+        this.view.ErrorOccured += OnError;
+    }
+
+    private void OnLoaded()
+    {
+        view.Render("Hello World");
+    }
+
+    private void OnError(string text)
+    {
+        log.LogError(text);
+    }
+}
