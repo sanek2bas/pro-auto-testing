96e98b806ddc017a43d0bb443cd94615b0bbef8b
sanek2bas <sanek_2bas@mail.ru>
2026-01-27 12:09:24 +0300

    Analyze_LoggerThrows_CallsWebService_With_NStub

--- Isolating_Frames_For_Generating_Dummy_Objects/LogAn.UnitTests/LogAnalyzer2Tests.cs
+++ Isolating_Frames_For_Generating_Dummy_Objects/LogAn.UnitTests/LogAnalyzer2Tests.cs
@@ -1,4 +1,8 @@
-﻿namespace LogAn.UnitTests;
+﻿using NSubstitute;
+using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace LogAn.UnitTests;
 
 public class LogAnalyzer2Tests
 {
@@ -16,5 +20,23 @@ public class LogAnalyzer2Tests
         analyzer2.Analyze(tooShortFileName);
 
         Assert.Contains("fake exception", mockWebService.MessageToWebService);
+    }
+
+    [Fact]
+    public void Analyze_LoggerThrows_CallsWebService_With_NStub()
+    {
+        var mockWebService = Substitute.For<IWebService>();
+        var stubLogger = Substitute.For<ILogger>();
+        stubLogger.When(
+            logger => logger.LogError(Arg.Any<string>()))
+            .Do(info => { throw new Exception("fake exception"); });
+
+        var analyzer = new LogAnalyzer2(stubLogger, mockWebService);
+
+        analyzer.MinNameLength = 10;
+        analyzer.Analyze("Short.txt");
+
+        mockWebService.Received()
+            .Write(Arg.Is<string>(s => s.Contains("fake exception")));
     }
 }
