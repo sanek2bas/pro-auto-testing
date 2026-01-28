b3cb5accd88785875cdb941376474b37a06c6f88
sanek2bas <sanek_2bas@mail.ru>
2026-01-28 15:33:49 +0300

    Sending_A_Greetings_Email

--- Unit_Testing_Styles/SUT.UnitTests/ControllerTests.cs
+++ Unit_Testing_Styles/SUT.UnitTests/ControllerTests.cs
@@ -1,11 +1,19 @@
-﻿namespace SUT.UnitTests;
+﻿using Moq;
+
+namespace SUT.UnitTests;
 
 public class ControllerTests
 {
-//    var emailGatewayMock = new Mock<IEmailGateway>();
-//    var sut = new Controller(emailGatewayMock.Object);
-//    sut.GreetUser("user@email.com");
-//emailGatewayMock.Verify(
-//x => x.SendGreetingsEmail("user@email.com"),
-//Times.Once);
+    [Fact]
+    public void Sending_A_Greetings_Email()
+    {
+        var emailGatewayMock = new Mock<IEmailGateway>();
+        var sut = new Controller(emailGatewayMock.Object);
+
+        sut.GreetUser("user@email.com");
+
+        emailGatewayMock.Verify(
+            x => x.SendGreetingsEmail("user@email.com"),
+            Times.Once);
+    }
 }
--- Unit_Testing_Styles/SUT/Controller.cs
+++ Unit_Testing_Styles/SUT/Controller.cs
@@ -2,13 +2,20 @@
 
 public interface IEmailGateway
 {
-
+    void SendGreetingsEmail(string email);
 }
 
 public class Controller
 {
+    private readonly IEmailGateway emailGateway;
+
+    public Controller(IEmailGateway gateway)
+    {
+        emailGateway = gateway;
+    }
+
     public void GreetUser(string email)
     {
-
+        emailGateway.SendGreetingsEmail(email);
     }
 }
