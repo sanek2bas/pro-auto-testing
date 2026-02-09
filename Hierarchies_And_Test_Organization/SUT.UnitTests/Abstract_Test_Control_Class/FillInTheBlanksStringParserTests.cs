109599a79e6ccf73ccd6d49233cbb3d8b821dcbb
sanek2bas <sanek_2bas@mail.ru>
2026-02-09 12:13:59 +0300

    Abstract_Test_Control_Class

---
+++ Hierarchies_And_Test_Organization/SUT.UnitTests/Abstract_Test_Control_Class/FillInTheBlanksStringParserTests.cs
@@ -0,0 +1,38 @@
+﻿using SUT.Template_Test_Class;
+
+namespace SUT.UnitTests.Abstract_Test_Control_Class;
+
+public abstract class FillInTheBlanksStringParserTests
+{
+    protected abstract IStringParser GetParser(string input);
+
+    protected abstract string HeaderVersion_SingleDigit { get; }
+    protected abstract string HeaderVersion_WithMinorVersion { get; }
+    protected abstract string HeaderVersion_WithRevision { get; }
+
+    public const string EXPECTED_SINGLE_DIGIT = "1";
+    public const string EXPECTED_WITH_REVISION = "1.1.1";
+    public const string EXPECTED_WITH_MINORVERSION = "1.1";
+
+    [Fact]
+    public void GetStringVersionFromHeader_SingleDigit_Found()
+    {
+        string input = HeaderVersion_SingleDigit;
+        
+        IStringParser parser = GetParser(input);
+        string versionFromHeader = parser.GetTextVersionFromHeader();
+
+        Assert.Equal(EXPECTED_SINGLE_DIGIT, versionFromHeader);
+    }
+
+    [Fact]
+    public void GetStringVersionFromHeader_WithRevision_Found()
+    {
+        string input = HeaderVersion_WithRevision;
+
+        IStringParser parser = GetParser(input);
+        string versionFromHeader = parser.GetTextVersionFromHeader();
+
+        Assert.Equal(EXPECTED_WITH_REVISION, versionFromHeader);
+    }
+}
---
+++ Hierarchies_And_Test_Organization/SUT.UnitTests/Abstract_Test_Control_Class/StandardStringParserTests.cs
@@ -0,0 +1,20 @@
+﻿using SUT.Template_Test_Class;
+
+namespace SUT.UnitTests.Abstract_Test_Control_Class;
+
+public class StandardStringParserTests : FillInTheBlanksStringParserTests
+{
+    protected override string HeaderVersion_SingleDigit =>
+        string.Format("header\tversion ={0}\t\n", EXPECTED_SINGLE_DIGIT);
+
+    protected override string HeaderVersion_WithMinorVersion =>
+        string.Format("header\tversion ={0}\t\n", EXPECTED_WITH_MINORVERSION);
+
+    protected override string HeaderVersion_WithRevision =>
+        string.Format("header\tversion ={0}\t\n", EXPECTED_WITH_REVISION);
+
+    protected override IStringParser GetParser(string input)
+    {
+        return new StandardStringParser(input);
+    }
+}
