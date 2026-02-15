96a0001c9d4efbb2abbceb5c60e6399c24f08573
sanek2bas <sanek_2bas@mail.ru>
2026-02-15 22:57:21 +0300

    adde existing project

---
+++ Integration_Testing/CRM.IntegrationTests/UserControllerIntegrationTests.cs
@@ -0,0 +1,10 @@
+﻿namespace CRM.IntegrationTests;
+
+public class UserControllerIntegrationTests
+{
+    [Fact]
+    public void Change_Email_From_Corporate_To_Non_Corporate()
+    {
+        
+    }
+}
